742e9d213ec89bd4f2a68222a38356e611b57a91
Incept1on <incept1onnn999@gmail.com>
2025-10-08 21:37:50 +0300

    add backend for terraform state in s3

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,7 +4,23 @@ terraform {
       source = "yandex-cloud/yandex"
     }
   }
+
   required_version = ">= 0.13"
+
+  backend "s3" {
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+    bucket = "udp-joker-tfstate"
+    region = "ru-central1"
+    key    = "./terraform.tfstate"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true # This option is required for Terraform 1.6.1 or higher.
+    skip_s3_checksum            = true # This option is required to describe a backend for Terraform 1.6.3 or higher.
+
+  }
 }
 
 provider "yandex" {
